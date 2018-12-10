fc250c7af0712f01524789021e1cd05436c2af1b
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-12-10 18:36:20 +0200

    ready for tests

--- contracts/Pan.sol
+++ contracts/Pan.sol
@@ -1,6 +1,22 @@
 pragma solidity 0.4.24;
 
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract Pan {
 
+contract Pan is ERC20Mintable {
+
+    string public constant name = "Pandora AI Network Token";
+    string public constant symbol = "PAN";
+    uint public constant decimals = 18;
+
+    /**
+     * @dev Pan token constructor
+     * @param _totalSupply Total amount of tokens that should be minted at contract creation
+     * @notice All minted tokens will be transferred to the token creator 
+     * and this amount will be allowed to be spent by the him
+     */
+    constructor (uint256 _totalSupply) public {
+        mint(msg.sender, _totalSupply);
+        approve(msg.sender, _totalSupply);
+    }
 }
