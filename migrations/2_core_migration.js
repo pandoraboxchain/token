4a4cad325a473e00bcce0b9c3f09998575021e0b
Kostiantyn Smyrnov <kostysh@gmail.com>
2018-12-12 06:55:22 +0200

    migration script updated with wallet deployment configuration

--- migrations/2_core_migration.js
+++ migrations/2_core_migration.js
@@ -1,10 +1,30 @@
 const { checkBalance } = require('./helpers/utils');
 const Pan = artifacts.require('Pan');
+const MultiSigWallet = artifacts.require('MultiSigWallet');
 
 module.exports = (deployer, network, accounts) => {
-    
+
+    let pan;
+    let wallet;
+        
     return deployer
-        .then(_ => deployer.deploy(Pan, 5000000))
+        .then(_ => deployer.deploy(Pan, 0))
+        .then(_ => Pan.deployed())
+        .then(instance => {
+            pan = instance;
+            return deployer.deploy(MultiSigWallet, [accounts[0]], 1);
+        })
+        .then(_ => MultiSigWallet.deployed())
+        .then(instance => {
+            wallet = instance;
+            return pan.addMinter(wallet.address, { from: accounts[0] });
+        })
+        .then(_ => pan.renounceMinter({ from: accounts[0] }))
+        .then(_ => wallet.send(web3.toWei(5, 'ether'), { from: accounts[0] }))
+        .then(_ => {
+            const mintData = pan.contract.mint.getData(wallet.address, 5000000);
+            return wallet.submitTransaction(pan.address, 0, mintData, { from: accounts[0] });
+        })
         .then(_ => checkBalance(web3, accounts[0]))
         .then(balance => {
             
